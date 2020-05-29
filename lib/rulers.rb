c96533735065ec2b887a0cae61aab339626d98ab
Jon Sumulong <sumulong.jon@gmail.com>
2020-05-29 13:38:37 -0500

    add logging

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,7 @@ module Rulers
   class Error < StandardError; end
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
     end
   end
