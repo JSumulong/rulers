1732b4dfe2c02ee25903bbc836db428dfac757e1
Jon Sumulong <sumulong.jon@gmail.com>
2020-05-29 13:51:24 -0500

    remove rulers-0.0.1.gem

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,7 +4,6 @@ module Rulers
   class Error < StandardError; end
   class Application
     def call(env)
-      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
     end
   end
